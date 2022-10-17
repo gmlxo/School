f1c247c0de9ad025c07349ab4f699cc9290c6327
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-10-17 09:42:19 +0900

    Update DTO_DB.sql

--- DTO/DTOProject/src/main/webapp/sql/DTO_DB.sql
+++ DTO/DTOProject/src/main/webapp/sql/DTO_DB.sql
@@ -7,9 +7,9 @@ book_loc VARCHAR2(20)
 CREATE SEQUENCE book_seq;
 
 INSERT INTO book VALUES (book_seq.NEXTVAL, 'book1', '001-00001');
-INSERT INTO book VALUES (book_seq.NEXTVAL, 'book1', '002-00002');
-INSERT INTO book VALUES (book_seq.NEXTVAL, 'book1', '003-00003');
-INSERT INTO book VALUES (book_seq.NEXTVAL, 'book1', '004-00004');
-INSERT INTO book VALUES (book_seq.NEXTVAL, 'book1', '005-00005');
+INSERT INTO book VALUES (book_seq.NEXTVAL, 'book2', '002-00002');
+INSERT INTO book VALUES (book_seq.NEXTVAL, 'book3', '003-00003');
+INSERT INTO book VALUES (book_seq.NEXTVAL, 'book4', '004-00004');
+INSERT INTO book VALUES (book_seq.NEXTVAL, 'book5', '005-00005');
 
 commit;
