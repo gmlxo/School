5d0dccf8b14613dabe3661df73e7329c0c4bb91d
gmlxo <87000854+gmlxo@users.noreply.github.com>
2022-11-28 12:42:56 +0900

    .

---
+++ Shopping/ShoppingProject/src/main/java/dao/MemberDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	String sql;
+	
+}
